47aeb78d10bbd21d49ece04854ed230f84ef0373
Ruben Perez <49155114+rubenperezf@users.noreply.github.com>
2019-10-03 17:39:39 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+function rich() {
+    alert("WRONG!")
+}
+function famous() {
+    alert("NOPE!")
+}
+function poor() {
+    alert("You got it!")
+}
+function mansion() {
+    alert("hahahahaha. Almost!")
+}
+function fit() {
+    alert("No way!")
+}
+function car() {
+    alert("I love your car!")
+}
